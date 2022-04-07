9366bd8d7cc5c19ce989f2f348f7476db9259a1f
ZenRool <lorenzopapperini@hotmail.it>
2022-04-07 13:27:24 +0200

    Chiedo l'età e i chilometri, converto tutto in numeri

---
+++ js/script.js
@@ -0,0 +1,3 @@
+// Chiedo i chilometri da percorrere e l'età del passeggiero e converto tutto
+const km = parseInt (prompt ("Quanti chilometri vuoi percorrere?"));
+const age = parseInt (prompt ("Quanti anni hai?"));
