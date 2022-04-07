0e3934afa7f45e1d96dae130064a7d026745ca49
ZenRool <lorenzopapperini@hotmail.it>
2022-04-07 17:50:41 +0200

    Sistemata semantica e logica js, aggiunti operatori terziari per il risultato

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
 const priceKm = 0.21;
-let discount = "";
+let discountTicket = null;
 // Chiedo i chilometri da percorrere e l'età del passeggiero e converto tutto
 const km = parseInt (prompt ("Quanti chilometri vuoi percorrere?"));
 const age = parseInt (prompt ("Quanti anni hai?"));
 // assegnio il prezzo
-let priceTicket = priceKm * km;
+const priceTicket = priceKm * km;
 // modifico il pezzo in base allo sconto 
 if (age < 18) {
-    discount = `${priceTicket} € meno il 20% per i minori <br>`;
-    priceTicket = priceTicket - ( priceTicket / 5);
+    discountTicket  = priceTicket * 0.8;
 }
 else if (age >= 65){
-    discount = `${priceTicket} € meno il 40% per lo sconto anziani <br>`;
-    priceTicket = priceTicket - ( priceTicket / 2.5);
+    discountTicket  = priceTicket * 0.6;
 }
-document.getElementById("receipt").innerHTML = `Il prezzo del biglietto è di ${discount} ${priceTicket.toFixed(2)} €`;
+
+document.getElementById("receipt").innerHTML = `Per il viaggio di ${km}km Il prezzo del biglietto è di ${priceTicket.toFixed(2)}€${discountTicket ? ` <br>meno il  ${age < 18 ? (`20% per i minori `) : (`40% per gli anziani `)} ${discountTicket.toFixed(2)}€` : (``)}` ;
