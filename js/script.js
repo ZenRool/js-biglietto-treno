bd4330d1eadfb1061e9a071769c093aaf69d2d4d
ZenRool <lorenzopapperini@hotmail.it>
2022-04-07 13:50:17 +0200

    Calcolo l'età e il relativo sconto

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+const priceKm = 0.21;
+let discount = "";
 // Chiedo i chilometri da percorrere e l'età del passeggiero e converto tutto
 const km = parseInt (prompt ("Quanti chilometri vuoi percorrere?"));
 const age = parseInt (prompt ("Quanti anni hai?"));
+// assegnio il prezzo
+let priceTicket = priceKm * km;
+// modifico il pezzo in base allo sconto 
+if (age < 18) {
+    discount = `${priceTicket} meno il 20%`;
+    priceTicket = priceTicket - ( priceTicket / 5);
+}
+else if (age >= 65){
+    discount = `${priceTicket} meno il 40%`;
+    priceTicket = priceTicket - ( priceTicket / 2.5);
+}
